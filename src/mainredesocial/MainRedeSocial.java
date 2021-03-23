103f7f805f0745370baf5c5b255a81faf0f53376
55819 <55819@DESKTOP-NQIRG9J>
2021-03-23 14:40:56 -0300

    Primeiro commit UNIKUT

---
+++ src/mainredesocial/MainRedeSocial.java
@@ -0,0 +1,21 @@
+
+package mainredesocial;
+
+
+public class MainRedeSocial {
+
+    
+    public static void main(String[] args) {
+    
+   
+    
+    }
+    
+    public void loginOuCadastro (){
+        System.out.println("Menu de opções:");
+        System.out.println("1-Entrar na conta.");
+        System.out.println("2-Cadastrar conta.");
+        System.out.println("3-Encerrar o programa.");
+        System.out.println("Escolha uma opção:");
+    }
+}
